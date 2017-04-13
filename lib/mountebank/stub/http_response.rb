cd4edbcfb8354e784376c92f25cff9d2f8829f68
Rory McKinley <rorymckinley@gmail.com>
2017-04-13 08:06:18 +0200

    Cheap and dirty behaviours support
    * No tests - just a dirty, filthy hack

--- lib/mountebank/stub/http_response.rb
+++ lib/mountebank/stub/http_response.rb
@@ -1,11 +1,11 @@
 class Mountebank::Stub::HttpResponse < Mountebank::Stub::Response
-  def self.create(statusCode=200, headers={}, body='')
+  def self.create(statusCode=200, headers={}, body='', behaviours={})
     payload = {}
     payload[:statusCode] = statusCode
     payload[:headers] = headers unless headers.empty?
     payload[:body] = body unless body.empty?
 
-    data = {is: payload}
+    data = {is: payload, _behaviours: behaviours}
     new(data)
   end
 end
