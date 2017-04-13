8f9d20e227ee8538508f3be940597d70b90b6243
Rory McKinley <rorymckinley@gmail.com>
2017-04-13 08:20:41 +0200

    Proper implementation this time
    * Also, 'Muricanise behaviours

--- lib/mountebank/stub/http_response.rb
+++ lib/mountebank/stub/http_response.rb
@@ -1,11 +1,11 @@
 class Mountebank::Stub::HttpResponse < Mountebank::Stub::Response
-  def self.create(statusCode=200, headers={}, body='', behaviours={})
+  def self.create(statusCode=200, headers={}, body='', behaviors={})
     payload = {}
     payload[:statusCode] = statusCode
     payload[:headers] = headers unless headers.empty?
     payload[:body] = body unless body.empty?
 
-    data = {is: payload, _behaviours: behaviours}
+    data = {is: payload, behaviors: behaviors}
     new(data)
   end
 end
